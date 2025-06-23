01096ce24c2cdc3c602ff68976326bfb557fc2c0
italo macedo <macedoitalo21@outlook.com>
2025-06-22 23:09:40 -0300

    feat: endpoint de cadastro de matricula de usuario

---
+++ Application/DTOs/Admin/Registration/CreateRegistrationDTO.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Application.DTOs.Admin.Registration;
+
+public class CreateRegistrationDTO
+{
+    public string RegistrationStatus { get; set; } = null!;
+    public DateTime RegistrationDate { get; set; }
+    public DateTime? CancellationDate { get; set; }
+    public required ClassIdDTO Class { get; set; }
+    public required UserIdDTO User { get; set; }
+    public string? VindiPlanId { get; set; }
+}
+
+public class UserIdDTO
+{
+   public Guid Id { get; set; }
+}
+
+public class ClassIdDTO
+{
+    public Guid Id { get; set; }
+
+}
+
+public class CourseIdDTO
+{
+    public Guid Id { get; set; }
+}
